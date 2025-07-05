409551066d3b2cca9e7100dce2a46c7c92051085
Shamsur Rahman Rifat <70819511+shamsur-rahman-rifat@users.noreply.github.com>
2025-07-05 12:33:23 +0600

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Loader from '../loader/loader';
+import Loader from '../loader/Loader';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
